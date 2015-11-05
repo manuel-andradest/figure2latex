c8c8842e5e279c4d152b2d02bbfbb004d0a373d8
manuel-andradest <manuel.andradest@uanl.edu.mx>
2015-11-04 19:25:55 -0600

    First really working file

--- figure2latex.m
+++ figure2latex.m
@@ -66,6 +66,8 @@ if ~replace
     end
 end
 
+fontsize = [10 9];
+
 [fid,errmsg] = fopen(fileout,'wt','n','UTF-8');
 
 if fid == -1
@@ -80,12 +82,11 @@ tab = '    ';
 
 
 fprintf(fid,'%% This file was created using FIGURE2LATEX for MATLAB\n%% %s %s\n\n',datestr(now,'yyyy/mm/dd HH:MM:SS'),filename);
-fprintf(fid,'\\documentclass{minimal}\n\n');
-    
-children = findobj(h,'type','axes');
+fprintf(fid,'\\documentclass[%dpt]{article}\n\n',fontsize(1));
+fprintf(fid,'\\usepackage{amsmath,mathptmx}\n\n');
 
+children = findobj(h,'type','axes');
 n = length(children);
-paperwidth = 345;
 
 xlabels = get(children,'Xlabel');
 ylabels = get(children,'Ylabel');
@@ -96,34 +97,49 @@ hf = figure;
 ha = axes;
 
 extents = zeros(2*n,4);
+textents = zeros(n,2);
+xticks = zeros(n,2);
+
+xticks(:,1) = cellfun(@min,get(children,'Xtick'));
+xticks(:,2) = cellfun(@max,get(children,'Xtick'));
+yticks = cell2mat(get(children,'Ytick'));
+
 
 for k = 1:n
-    ht = text(0,0,xtext{k},'FontName','Times','FontSize',9,'Units','Points','Parent',ha);
+    ht = text(0,0,xtext{k},'FontName','Times','FontSize',fontsize(1),'Units','Points','Parent',ha);
     extents(k,:) = get(ht,'Extent');
-    ht = text(0,0,ytext{k},'FontName','Times','FontSize',9,'Units','Points','Parent',ha);
+    ht = text(0,0,ytext{k},'FontName','Times','FontSize',fontsize(1),'Units','Points','Parent',ha);
     extents(k+n,:) = get(ht,'Extent');
+    ht = text(0,0,strrep(num2str(xticks(n,:)),' ',''),'FontName','Times','FontSize',fontsize(2),'Units','Points','Parent',ha);
+    extent = get(ht,'Extent');
+    textents(k,1) = extent(3)/2;
+    ht = text(0,0,num2str(yticks(n,:).'),'FontName','Times','FontSize',fontsize(2),'Units','Points','Parent',ha);
+    extent = get(ht,'Extent');
+    textents(k,2) = extent(3);
 end
 
-ht = text(0,0,'X','FontName','Times','FontSize',9,'Units','Points','Parent',ha);
 close(hf);
 
 [~,mxlabeli] = max(extents(1:n,4));
+maxxlabel = xtext{mxlabeli};
 [~,mylabeli] = max(extents(n+1:end,4));
-
-mxlabel = xtext{mxlabeli};
-mylabel = ytext{mylabeli};
+maxylabel = ytext{mylabeli};
 
 exlabel = cellfun(@isempty,xtext);
 eylabel = cellfun(@isempty,ytext);
 
-if all(exlabel)
-    xtext{exlabel} = '\phantom{X}';
-end
+textent = round(max(textents,[],1));
+lextent = round([max(extents(1:n,4)) max(extents(n+1:end,4))]);
 
-if all(eylabel)
-    ytext{eylabel} = '\phantom{X}';
+for k = 1:n
+    if ~all(exlabel) && exlabel(k)
+        xtext{k} = '\vphantom{X}';
+    end
+    if ~all(eylabel) && eylabel(k)
+        ytext{k} = sprintf('\\vphantom{%s}',maxylabel);
+    end
 end
-
+    
 switch n
     case 1
         ratio = inv((sqrt(5)+1)/2);
@@ -133,17 +149,52 @@ switch n
         ratio = sqrt(2);
 end
 
+paperwidth = 345;
 paperheight = round(paperwidth*ratio);
+axesheight = round(paperheight/n-lextent(1)-fontsize(2)*2.5-11);
+axeswidth = round(paperwidth-sum(textent)-lextent(2)-11);
+paperheight = axesheight*n;
 
 fprintf(fid,'\\usepackage[papersize={%dpt,%dpt},body={%dpt,%dpt}]{geometry}\n',...
     paperwidth,paperheight,paperwidth,paperheight);
 fprintf(fid,'\\usepackage{tikz}\n\n');
 fprintf(fid,'\\usetikzlibrary{datavisualization}\n');
-fprintf(fid,'\\setlength\\parindent{0pt}\n\n');
+fprintf(fid,'\\setlength\\parindent{0pt}\n\\pagestyle{empty}\n\n');
 
-fprintf(fid,'\\begin{document}\n%s\n\\end{document}','Hola');
+fprintf(fid,'\\begin{document}\n');
+
+for k = n:-1:1
+  fprintf(fid,'\\begin{tikzpicture}\n\\datavisualization [\n');
+  fprintf(fid,'scientific axes={width=%dpt,height=%dpt,inner ticks},\n',axeswidth,axesheight);
+  fprintf(fid,'x axis={attribute=xdata, label={%s}},\n',latexfy(xtext{k}));
+  fprintf(fid,'y axis={attribute=ydata, label={%s}},\n',latexfy(ytext{k}));
+  fprintf(fid,'visualize as smooth line\n]');
+  fprintf(fid,'data {\nxdata, ydata\n');
+  lh = findobj(children(k),'type','line');
+  xdata = get(lh(1),'XData');
+  for m = 1:1
+      ydata = get(lh(m),'YData');
+  end
+  data = [xdata;ydata];
+  fprintf(fid,'%f,%f\n',data);
+  if k > 1
+      newline = '\\[-1pt]';
+  else
+      newline = '';
+  end
+  fprintf(fid,'};\n\\end{tikzpicture}%s\n',newline);
+end
+
+fprintf(fid,'\n\\end{document}');
 
 if fclose(fid) == -1
     error('figure2latex:FileOperation',...
         ferror(fid));
 end
+
+function s = latexfy(s)
+
+if regexpi(s,'[_^]')
+    s = sprintf('$%s$',s);
+end
+
